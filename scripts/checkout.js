22b8f02ec4637cacc33e42f560953896edc423e2
Prajwal Kuchewar <prajwalkuchewar3@gmail.com>
2024-12-16 15:35:07 +0530

    delete products from html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,7 +14,7 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
     <div class="cart-item-details-grid">
@@ -95,5 +95,7 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click', () => {
     const { productId } = link.dataset;
     removeFromCart(productId);
+
+    document.querySelector(`.js-cart-item-container-${productId}`).remove();
   });
 });
